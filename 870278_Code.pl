a8556586388f932acb5f4a90f33f44422b59d27f
Stuart Kenward <870278@swansea.ac.uk>
2020-04-19 20:31:22 +0100

    adding grammatical components

--- 870278_Code.pl
+++ 870278_Code.pl
@@ -1,48 +1,47 @@
-/*pronouns*/
-pro(i).
-pro(you).
-pro(he).
-pro(she).
-pro(it).
-pro(we).
-pro(you).
-pro(they).
-pro(me).
-pro(you).
-pro(him).
-pro(her).
-pro(it).
-pro(it).
-pro(us).
-pro(you).
-pro(them).
+/*pronouns(word, singular/plural, 1st 2nd or 3rd person, grammatical role(object or subject))*/
+pro(i,singular,1,subject).
+pro(you,singular,2,subject).
+pro(he,singular,3,subject).
+pro(she,singular,3,subject).
+pro(it,singular,3,subject).
+pro(we,plural,1,subject).
+pro(you,plural,2,subject).
+pro(they,plural,3,subject).
+pro(me,singular,1,object).
+pro(you,singular,2,object).
+pro(him,singular,3,object).
+pro(her,singular,3,object).
+pro(it,singular,3,object).
+pro(us,plural,1,object).
+pro(you,plural,2,object).
+pro(them,plural,3,object).
 
-/*verbs*/
-v(know).
-v(know).
-v(knows).
-v(know).
-v(see).
-v(see).
-v(sees).
-v(see).
+/*verbs(word, singular/plural, 1st 2nd or 3rd person)*/
+v(know,singular,1).
+v(know,singular,2).
+v(knows,singular,3).
+v(know,plural,_).
+v(see,singular,1).
+v(see,singular,2).
+v(sees,singular,3).
+v(see,plural,_).
 
-/*determiner*/
-det(the).
-det(a).
-det(two).
+/*determiner(word, singular/plural)*/
+det(the,_).
+det(a,singular).
+det(two,plural).
 
-/*nouns*/
-n(man).
-n(woman).
-n(apple).
-n(chair).
-n(room).
-n(men).
-n(women).
-n(apples).
-n(chairs).
-n(rooms).
+/*nouns(word, singular/plural)*/
+n(man,singular).
+n(woman,singular).
+n(apple,singular).
+n(chair,singular).
+n(room,singular).
+n(men,plural).
+n(women,plural).
+n(apples,plural).
+n(chairs,plural).
+n(rooms,plural).
 
 /*prepositions*/
 prep(on).
