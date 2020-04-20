4c9c9e7db0a56e1e95205378a47434d9fbc6f661
Stuart Kenward <870278@swansea.ac.uk>
2020-04-20 17:02:35 +0100

    Adding pronoun subjects vs objects

--- 870278_Code.pl
+++ 870278_Code.pl
@@ -1,5 +1,5 @@
 /*pronouns(word, singular/plural, 1st 2nd or 3rd person, grammatical role(object or subject))*/
-pro(i,singular,1,subject).
+/*pro(i,singular,1,subject).
 pro(you,singular,2,subject).
 pro(he,singular,3,subject).
 pro(she,singular,3,subject).
@@ -14,25 +14,50 @@ pro(her,singular,3,object).
 pro(it,singular,3,object).
 pro(us,plural,1,object).
 pro(you,plural,2,object).
-pro(them,plural,3,object).
+pro(them,plural,3,object).*/
+
+pro(subject) --> [i].
+pro(subject) --> [you].
+pro(subject) --> [he].
+pro(subject) --> [she].
+pro(subject) --> [it].
+pro(subject) --> [we].
+pro(subject) --> [you].
+pro(subject) --> [they].
+pro(object) --> [me].
+pro(object) --> [you].
+pro(object) --> [him].
+pro(object) --> [her].
+pro(object) --> [it].
+pro(object) --> [us].
+pro(object) --> [you].
+pro(object) --> [them].
 
 /*verbs(word, singular/plural, 1st 2nd or 3rd person)*/
-v(know,singular,1).
+/*v(know,singular,1).
 v(know,singular,2).
 v(knows,singular,3).
 v(know,plural,_).
 v(see,singular,1).
 v(see,singular,2).
 v(sees,singular,3).
-v(see,plural,_).
+v(see,plural,_).*/
+
+v --> [know].
+v --> [see].
+v --> [knows]. 
+v --> [sees].
 
 /*determiner(word, singular/plural)*/
-det(the,_).
+/*det(the,_).
 det(a,singular).
-det(two,plural).
+det(two,plural).*/
+det --> [the].
+det --> [a].
+det --> [two].
 
 /*nouns(word, singular/plural)*/
-n(man,singular).
+/*n(man,singular).
 n(woman,singular).
 n(apple,singular).
 n(chair,singular).
@@ -41,25 +66,39 @@ n(men,plural).
 n(women,plural).
 n(apples,plural).
 n(chairs,plural).
-n(rooms,plural).
+n(rooms,plural).*/
+
+n --> [man].
+n --> [woman].
+n --> [apple].
+n --> [chair].
+n --> [room].
+n --> [men].
+n --> [women].
+n --> [apples].
+n --> [chairs].
+n --> [rooms].
 
 /*prepositions*/
-prep(on).
-prep(in).
-prep(under).
+prep --> [on].
+prep --> [in].
+prep --> [under].
 
 /*adjectives*/
-adj(old).
-adj(young).
-adj(red).
-adj(short).
-adj(tall).
+adj --> [old].
+adj --> [young].
+adj --> [red].
+adj --> [short].
+adj --> [tall].
 
 /*rules*/
-s(X,Y) :-
-    pro(X,_,_,_),
-    v(Y,_,_).
-
-s(X,Y) :-
-    det(X,singular),
-    n(Y,singular).
+s --> np(subject),vp.
+np(_) --> det,nbar.
+np(X) --> pro(X).
+/*np --> det,nbar,pp.*/
+nbar --> n.
+nbar --> jp.
+jp --> adj, jp.
+jp --> adj, n.
+pp --> prep,np(_).
+vp --> v, np(_).
