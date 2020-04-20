41b06234d07288d19c0b8f7386b0b1380cf32245
Stuart Kenward <870278@swansea.ac.uk>
2020-04-20 17:14:37 +0100

    Adding correct syntax (logic needs fixing)

--- 870278_Code.pl
+++ 870278_Code.pl
@@ -1,83 +1,50 @@
 /*pronouns(word, singular/plural, 1st 2nd or 3rd person, grammatical role(object or subject))*/
-/*pro(i,singular,1,subject).
-pro(you,singular,2,subject).
-pro(he,singular,3,subject).
-pro(she,singular,3,subject).
-pro(it,singular,3,subject).
-pro(we,plural,1,subject).
-pro(you,plural,2,subject).
-pro(they,plural,3,subject).
-pro(me,singular,1,object).
-pro(you,singular,2,object).
-pro(him,singular,3,object).
-pro(her,singular,3,object).
-pro(it,singular,3,object).
-pro(us,plural,1,object).
-pro(you,plural,2,object).
-pro(them,plural,3,object).*/
 
-pro(subject) --> [i].
-pro(subject) --> [you].
-pro(subject) --> [he].
-pro(subject) --> [she].
-pro(subject) --> [it].
-pro(subject) --> [we].
-pro(subject) --> [you].
-pro(subject) --> [they].
-pro(object) --> [me].
-pro(object) --> [you].
-pro(object) --> [him].
-pro(object) --> [her].
-pro(object) --> [it].
-pro(object) --> [us].
-pro(object) --> [you].
-pro(object) --> [them].
+pro(singular,subject,1) --> [i].
+pro(singular,subject,2) --> [you].
+pro(singular,subject,3) --> [he].
+pro(singular,subject,3) --> [she].
+pro(singular,subject,3) --> [it].
+pro(plural,subject,1) --> [we].
+pro(plural,subject,2) --> [you].
+pro(plural,subject,3) --> [they].
+pro(singular,object,1) --> [me].
+pro(singular,object,2) --> [you].
+pro(singular,object,3) --> [him].
+pro(singular,object,3) --> [her].
+pro(singular,object,3) --> [it].
+pro(plural,object,1) --> [us].
+pro(plural,object,2) --> [you].
+pro(plural,object,3) --> [them].
 
 /*verbs(word, singular/plural, 1st 2nd or 3rd person)*/
-/*v(know,singular,1).
-v(know,singular,2).
-v(knows,singular,3).
-v(know,plural,_).
-v(see,singular,1).
-v(see,singular,2).
-v(sees,singular,3).
-v(see,plural,_).*/
 
-v --> [know].
-v --> [see].
-v --> [knows]. 
-v --> [sees].
+v(singular,1) --> [know].
+v(singular,2) --> [know].
+v(singular,3) --> [knows]. 
+v(plural,_) --> [know].
+v(singular,1) --> [see].
+v(singular,2) --> [see].
+v(singular,3) --> [sees].
+v(plural,_) --> [see].
 
 /*determiner(word, singular/plural)*/
-/*det(the,_).
-det(a,singular).
-det(two,plural).*/
-det --> [the].
-det --> [a].
-det --> [two].
+det(_) --> [the].
+det(singular) --> [a].
+det(plural) --> [two].
 
 /*nouns(word, singular/plural)*/
-/*n(man,singular).
-n(woman,singular).
-n(apple,singular).
-n(chair,singular).
-n(room,singular).
-n(men,plural).
-n(women,plural).
-n(apples,plural).
-n(chairs,plural).
-n(rooms,plural).*/
 
-n --> [man].
-n --> [woman].
-n --> [apple].
-n --> [chair].
-n --> [room].
-n --> [men].
-n --> [women].
-n --> [apples].
-n --> [chairs].
-n --> [rooms].
+n(singular) --> [man].
+n(singular) --> [woman].
+n(singular) --> [apple].
+n(singular) --> [chair].
+n(singular) --> [room].
+n(plural) --> [men].
+n(plural) --> [women].
+n(plural) --> [apples].
+n(plural) --> [chairs].
+n(plural) --> [rooms].
 
 /*prepositions*/
 prep --> [on].
@@ -93,12 +60,12 @@ adj --> [tall].
 
 /*rules*/
 s --> np(subject),vp.
-np(_) --> det,nbar.
-np(X) --> pro(X).
+np(_) --> det(_),nbar.
+np(X) --> pro(X,_,_).
 /*np --> det,nbar,pp.*/
-nbar --> n.
+nbar --> n(_).
 nbar --> jp.
 jp --> adj, jp.
-jp --> adj, n.
+jp --> adj, n(_).
 pp --> prep,np(_).
-vp --> v, np(_).
+vp --> v(_,_), np(_).
